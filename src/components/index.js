efb5c2dfe792018f0c932701f3477f0d1282ef99
paritosh rawat <paritoshrawat21@gmail.com>
2024-01-04 10:30:41 +0530

    appwrite

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Select from "./Select";
+import Input from "./Input";
+import PostCard from "./PostCard";
+import Button from "./Button";
+import RTE from "./RTE";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import AuthLayout from "./AuthLayout";
+
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Select,
+  Input,
+  PostCard,
+  Button,
+  RTE,
+  Signup,
+  Login,
+  PostForm,
+  AuthLayout,
+};
